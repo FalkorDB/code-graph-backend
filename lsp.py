5b204d1f458ae8f0f4fa75b1fa575defc4705018
Avi Avni <avi.avni@gmail.com>
2025-01-26 21:00:37 +0200

    refactor

--- lsp.py
+++ lsp.py
@@ -1,6 +1,6 @@
 import tqdm
 from pathlib import Path
-from typing import Optional, Self
+from typing import Callable, Optional, Self
 import tree_sitter_java
 from tree_sitter import Language, Parser, Node, Point, Tree
 from multilspy import SyncLanguageServer
@@ -10,140 +10,107 @@ from falkordb import FalkorDB
 
 config = MultilspyConfig.from_dict({"code_language": "java"})
 logger = MultilspyLogger()
-lsp = SyncLanguageServer.create(config, logger, "/Users/aviavni/repos/JFalkorDB")
+lsp = SyncLanguageServer.create(config, logger, "/Users/aviavni/repos/jedis")
 
 JAVA_LANGUAGE = Language(tree_sitter_java.language())
 parser = Parser(JAVA_LANGUAGE)
-files = {}
 
-class Method:
+class Entity:
     def __init__(self, node: Node):
         self.node = node
-        self.calls: list[Node] = []
-        self.resolved_calls: set[Method] = set()
-        self.parent = None
+        self.parent: Optional[Self] = None
+        self.symbols: dict[str, list[Node]] = {}
+        self.resolved_symbols: dict[str, set[Self]] = {}
 
-    def add_call(self, method: Node):
-        self.calls.append(method)
+    def add_symbol(self, key: str, symbol: Node):
+        if key not in self.symbols:
+            self.symbols[key] = []
+        self.symbols[key].append(symbol)
 
-    def add_resolved_call(self, method: Self):
-        self.resolved_calls.add(method)
+    def add_resolved_symbol(self, key: str, symbol: Self):
+        if key not in self.resolved_symbols:
+            self.resolved_symbols[key] = set()
+        self.resolved_symbols[key].add(symbol)
 
-class Type:
-    def __init__(self):
-        self.methods: dict[Node, Method] = {}
-
-    def add_method(self, method: Method):
-        method.parent = self
-        self.methods[method.node] = method
+    def resolved_symbol(self, f: Callable[[str, Node], list[Self]]):
+        for key, symbols in self.symbols.items():
+            self.resolved_symbols[key] = set()
+            for symbol in tqdm.tqdm(symbols, f"Resolve {key}"):
+                for resolved_symbol in f(key, symbol):
+                    self.resolved_symbols[key].add(resolved_symbol)
 
 class File:
     def __init__(self, path: Path, tree: Tree):
         self.path = path
         self.tree = tree
-        self.types: dict[Node, Type] = {}
+        self.types: dict[Node, Entity] = {}
 
-    def add_type(self, type: Type):
+    def add_type(self, type: Entity):
         self.parent = self
         self.types[type.node] = type
-
-class Interface(Type):
-    def __init__(self, node: Node):
-        self.node = node
-        self.extends: list[Interface] = []
-        super().__init__()
-    
-    def add_extends_interface(self, interface: Node):
-        self.extends.append(interface)
-
-class Class(Type):
-    def __init__(self, node: Node):
-        self.node = node
-        self.interfaces: list[Interface] = []
-        self.resolved_interfaces: list[Interface] = []
-        self.base_class: Optional[Node] = None
-        self.resolved_base_class: Optional[Type] = None
-        super().__init__()
-    
-    def add_implement_interface(self, interface: Node):
-        self.interfaces.append(interface)
-
-    def add_resolved_interface(self, interface: Interface):
-        self.resolved_interfaces.append(interface)
-
-    def set_base_class(self, base_class: Node):
-        self.base_class = base_class
-
-    def set_resolved_base_class(self, base_class: Self):
-        self.resolved_base_class = base_class
-
-class Enum(Type):
-    def __init__(self, node: Node):
-        self.node = node
-        super().__init__()
 
 def find_parent(node: Node, parent_types: list) -> Node:
     while node.type not in parent_types:
         node = node.parent
     return node
 
-def find_calls(node:Node, method: Method):
+def find_calls(method: Entity):
     query = JAVA_LANGUAGE.query("(method_invocation) @reference.call")
-    captures = query.captures(node)
+    captures = query.captures(method.node)
     if 'reference.call' in captures:
         for caller in captures['reference.call']:
-            method.add_call(caller)
+            method.add_symbol("call", caller)
 
-
-def find_methods(node: Node, type: Type):
+def find_methods(type: Entity):
     query = JAVA_LANGUAGE.query("[(method_declaration) (constructor_declaration)] @definition.method")
-    captures = query.captures(node)
+    captures = query.captures(type.node)
     if 'definition.method' in captures:
         for method_dec in captures['definition.method']:
-            type.add_method(Method(method_dec))
-            find_calls(method_dec, type.methods[method_dec])
+            method = Entity(method_dec)
+            method.parent = type
+            type.add_resolved_symbol("methods", method)
+            find_calls(method)
 
-
-def find_type(node: Node, file: File):
+def find_type(file: File):
     query = JAVA_LANGUAGE.query("[(class_declaration) (interface_declaration) (enum_declaration)] @type")
-    captures = query.captures(node)
+    captures = query.captures(file.tree.root_node)
     if 'type' in captures:
         for type_dec in captures['type']:
             if type_dec.type == 'class_declaration':
-                type = Class(type_dec)
+                type = Entity(type_dec)
                 interfaces_query = JAVA_LANGUAGE.query("(super_interfaces (type_list (type_identifier) @interface))")
                 interfaces_captures = interfaces_query.captures(type_dec)
                 if 'interface' in interfaces_captures:
                     for interface in interfaces_captures['interface']:
-                        type.add_implement_interface(interface)
+                        type.add_symbol("implement_interface", interface)
                 base_class_query = JAVA_LANGUAGE.query("(superclass (type_identifier) @base_class)")
                 base_class_captures = base_class_query.captures(type_dec)
                 if 'base_class' in base_class_captures:
                     base_class = base_class_captures['base_class'][0]
-                    type.set_base_class(base_class)
+                    type.add_symbol("base_class", base_class)
             elif type_dec.type == 'interface_declaration':
-                type = Interface(type_dec)
+                type = Entity(type_dec)
                 query = JAVA_LANGUAGE.query("(extends_interfaces (type_list (type_identifier) @type))?")
                 extends_captures = query.captures(type_dec)
                 if 'type' in extends_captures:
                     for interface in extends_captures['type']:
-                        type.add_extends_interface(interface)
+                        type.add_symbol("extend_interface", interface)
             elif type_dec.type == 'enum_declaration':
-                type = Enum(type_dec)
+                type = Entity(type_dec)
             file.add_type(type)
-            find_methods(type_dec, type)
+            find_methods(type)
 
 def resolve(lsp: SyncLanguageServer, path: Path, node: Node) -> list[tuple[File, Node]]:
     return [(files[Path(location['absolutePath'])], files[Path(location['absolutePath'])].tree.root_node.descendant_for_point_range(Point(location['range']['start']['line'], location['range']['start']['character']), Point(location['range']['end']['line'], location['range']['end']['character']))) for location in lsp.request_definition(str(path), node.start_point.row, node.start_point.column) if Path(location['absolutePath']) in files]
 
-def resolve_type(lsp: SyncLanguageServer, path: Path, node: Node) -> list[Type]:
+def resolve_type(lsp: SyncLanguageServer, path: Path, node: Node) -> list[Entity]:
     res = []
     for file, resolved_node in resolve(lsp, path, node):
         type_dec = find_parent(resolved_node, ['class_declaration', 'interface_declaration', 'enum_declaration'])
         res.append(file.types[type_dec])
     return res
 
-def resolve_method(lsp: SyncLanguageServer, path: Path, node: Node) -> list[Method]:
+def resolve_method(lsp: SyncLanguageServer, path: Path, node: Node) -> list[Entity]:
     res = []
     for file, resolved_node in resolve(lsp, path, node):
         method_dec = find_parent(resolved_node, ['method_declaration', 'constructor_declaration', 'class_declaration', 'interface_declaration', 'enum_declaration'])
@@ -153,7 +120,9 @@ def resolve_method(lsp: SyncLanguageServer, path: Path, node: Node) -> list[Meth
         res.append(file.types[type_dec].methods[method_dec])
     return res
 
-for path in Path("/Users/aviavni/repos/JFalkorDB").rglob("*.java"):
+files: dict[Path, File] = {}
+
+for path in tqdm.tqdm(Path("/Users/aviavni/repos/jedis").rglob("*.java"), "Parse files"):
     if 'test' in str(path):
         continue
 
@@ -163,25 +132,21 @@ for path in Path("/Users/aviavni/repos/JFalkorDB").rglob("*.java"):
     file = File(path, tree)
     files[path] = file
 
-    find_type(tree.root_node, file)
+    find_type(file)
+
+def resolve_symbol(key: str, symbol: Node) -> Entity:
+    if key in ["implement_interface", "base_class", "extend_interface"]:
+        return resolve_type(lsp, path, symbol)
+    elif key in ["call"]:
+        return resolve_method(lsp, path, call)
+    else:
+        raise ValueError(f"Unknown key {key}")
 
 with lsp.start_server():
     for path, file in tqdm.tqdm(files.items(), "Resolve file"):
-        for type_dec, type in file.types.items():
-            if isinstance(type, Class):
-                type.resolved_interfaces.clear()
-                for interface in type.interfaces:
-                    for resolved_interface in resolve_type(lsp, path, interface):
-                        type.add_resolved_interface(resolved_interface)
-                if type.base_class:
-                    resolved_class = resolve_type(lsp, path, type.base_class)
-                    if len(resolved_class) == 1:
-                        type.set_resolved_base_class(resolved_class[0])
-            for method_dec, method in type.methods.items():
-                method.resolved_calls.clear()
-                for call in method.calls:
-                    for resolved_method in resolve_method(lsp, path, call):
-                        method.add_resolved_call(resolved_method)
+        for type_dec, type in tqdm.tqdm(file.types.items(), "Resolve type"):
+            if type.node.type == 'class_declaration':
+                type.resolved_symbol(resolve_symbol)
 
 graph = FalkorDB().select_graph("java")
 
@@ -202,21 +167,20 @@ for path, file in tqdm.tqdm(files.items(), "Save file hierarchy"):
     for i, (class_dec, class_obj) in enumerate(file.types.items()):
         params[f"type_name_{i}"] = class_dec.child_by_field_name('name').text.decode('utf-8')
         query += f"CREATE (file)-[:DECLARE]->(type_{i}:Type {{name: $type_name_{i}}})\n"
-        for j, (method_dec, method_obj) in enumerate(class_obj.methods.items()):
-            params[f"method_name_{i}_{j}"] = method_dec.child_by_field_name('name').text.decode('utf-8')
-            params[f"mathod_params_{i}_{j}"] = method_dec.child_by_field_name('parameters').text.decode('utf-8')
-            query += f"CREATE (type_{i})-[:HAS_METHOD]->(method_{i}_{j}:Method {{name: $method_name_{i}_{j}, parameters: $mathod_params_{i}_{j}}})\n"
+        if "methods" in class_obj.resolved_symbols:
+            for j, method_obj in enumerate(class_obj.resolved_symbols["methods"]):
+                params[f"method_name_{i}_{j}"] = method_obj.node.child_by_field_name('name').text.decode('utf-8')
+                params[f"mathod_params_{i}_{j}"] = method_obj.node.child_by_field_name('parameters').text.decode('utf-8')
+                query += f"CREATE (type_{i})-[:HAS_METHOD]->(method_{i}_{j}:Method {{name: $method_name_{i}_{j}, parameters: $mathod_params_{i}_{j}}})\n"
     res = graph.query(query, params)
     nodes_created += res.nodes_created
     relationships_created += res.relationships_created
 print(f"Created {nodes_created} nodes and {relationships_created} relationships")
 
-actual_calls = 0
-expected_calls = 0
 for path, file in tqdm.tqdm(files.items(), "Save calls"):
     for class_dec, class_obj in file.types.items():
-        if isinstance(class_obj, Class):
-            for interface in class_obj.resolved_interfaces:
+        if "implement_interface" in class_obj.resolved_symbols:
+            for interface in class_obj.resolved_symbols["implement_interface"]:
                 query = "MATCH (file:File {path: $path})\n"
                 params = {"path": str(path)}
                 query += f"MATCH (file)-[:DECLARE]->(type:Type {{name: $type_name}})\n"
@@ -226,36 +190,32 @@ for path, file in tqdm.tqdm(files.items(), "Save calls"):
                 query += f"MERGE (type)-[:IMPLEMENTS]->(interface)\n"
                 query += "RETURN *"
                 res = graph.query(query, params)
-            if class_obj.resolved_base_class:
-                query = "MATCH (file:File {path: $path})\n"
-                params = {"path": str(path)}
-                query += f"MATCH (file)-[:DECLARE]->(type:Type {{name: $type_name}})\n"
-                params["type_name"] = class_dec.child_by_field_name('name').text.decode('utf-8')
-                query += f"MATCH (base_class:Type {{name: $base_class_name}})\n"
-                params["base_class_name"] = class_obj.resolved_base_class.node.child_by_field_name('name').text.decode('utf-8')
-                query += f"MERGE (type)-[:EXTENDS]->(base_class)\n"
-                query += "RETURN *"
-                res = graph.query(query, params)
-        for method_dec, method_obj in class_obj.methods.items():
+        if "base_class" in class_obj.resolved_symbols:
+            query = "MATCH (file:File {path: $path})\n"
+            params = {"path": str(path)}
+            query += f"MATCH (file)-[:DECLARE]->(type:Type {{name: $type_name}})\n"
+            params["type_name"] = class_dec.child_by_field_name('name').text.decode('utf-8')
+            query += f"MATCH (base_class:Type {{name: $base_class_name}})\n"
+            params["base_class_name"] = class_obj.resolved_symbols["base_class"].node.child_by_field_name('name').text.decode('utf-8')
+            query += f"MERGE (type)-[:EXTENDS]->(base_class)\n"
+            query += "RETURN *"
+            res = graph.query(query, params)
+        for method_obj in class_obj.resolved_symbols["methods"]:
             params = {}
             query = "MATCH (file:File {path: $path})\n"
             params["path"] = str(path)
             query += f"MATCH (file)-[:DECLARE]->(type:Type {{name: $type_name}})\n"
             params[f"type_name"] = class_dec.child_by_field_name('name').text.decode('utf-8')
             query += f"MATCH (type)-[:HAS_METHOD]->(caller:Method {{name: $method_name, parameters: $mathod_params}})\n"
-            params[f"method_name"] = method_dec.child_by_field_name('name').text.decode('utf-8')
-            params[f"mathod_params"] = method_dec.child_by_field_name('parameters').text.decode('utf-8')
-            if len(method_obj.resolved_calls) == 0:
-                continue
-            for i, call in enumerate(method_obj.resolved_calls):
-                params[f"callee_class_name_{i}"] = call.parent.node.child_by_field_name('name').text.decode('utf-8')
-                params[f"callee_method_name_{i}"] = call.node.child_by_field_name('name').text.decode('utf-8')
-                params[f"callee_mathod_params_{i}"] = call.node.child_by_field_name('parameters').text.decode('utf-8')
-                query += f"MATCH (:Type {{name: $callee_class_name_{i}}})-[:HAS_METHOD]->(callee_{i}:Method {{name: $callee_method_name_{i}, parameters: $callee_mathod_params_{i}}})\n"
-                query += f"MERGE (caller)-[:CALLS]->(callee_{i})\n"
-                query += f"WITH file, type, caller\n"
+            params[f"method_name"] = method_obj.node.child_by_field_name('name').text.decode('utf-8')
+            params[f"mathod_params"] = method_obj.node.child_by_field_name('parameters').text.decode('utf-8')
+            if "call" in method_obj.resolved_symbols:
+                for i, call in enumerate(method_obj.resolved_symbols["call"]):
+                    params[f"callee_class_name_{i}"] = call.parent.node.child_by_field_name('name').text.decode('utf-8')
+                    params[f"callee_method_name_{i}"] = call.node.child_by_field_name('name').text.decode('utf-8')
+                    params[f"callee_mathod_params_{i}"] = call.node.child_by_field_name('parameters').text.decode('utf-8')
+                    query += f"MATCH (:Type {{name: $callee_class_name_{i}}})-[:HAS_METHOD]->(callee_{i}:Method {{name: $callee_method_name_{i}, parameters: $callee_mathod_params_{i}}})\n"
+                    query += f"MERGE (caller)-[:CALLS]->(callee_{i})\n"
+                    query += f"WITH file, type, caller\n"
             query += "RETURN *"
             res = graph.query(query, params)
-            actual_calls += res.relationships_created
-            expected_calls += len(method_obj.resolved_calls)
-print(actual_calls, expected_calls, actual_calls == expected_calls)
